d31723557660977f32ca20e8f951c1fd1fbda864
shnazzyhat <juicycap@gmail.com>
2014-10-08 15:53:35 -0400

    zxcv

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+       
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php ob_start(); ?>
+        <div class="container">
+            <div class="favorite">Donpain's home page</div>
+            <p></p>
+            <div id="liam">      
+            </div>
+            <div class="clearfix"></div>
+            <ul class="my-special-list">
+                <li><a href="http://google.com" target="_blank"><img src="http://lh3.ggpht.com/5IkkJ28jCJa-dMyDX3luN5onFJTRcnHMDgCq3JVjdZ0EliJLrvFRGLw04_z7phmvYJD9QWKE617CTbUoch52qg242SRSGk4taUpHfcW5Og=s660"></a></li>
+                <li><a href="https://www.facebook.com" target="_blank"><img src="http://www.rainyfrog.com/wp-content/uploads/2014/04/Facebook-Icon-1021x1024.png"></a></li>
+                <li><a href="//tumblr.com" target="_blank"><img src="https://secure.assets.tumblr.com/images/logo_page/img_logo_bluebg_2x.png"></a></li>
+                <li><a href="//watchanimeon.com" target="_blank"><img src="http://images.saiyanisland.com/data/526/Naruto-Logo.gif"></a></li>
+                <li><a href="https://www.youtube.com/feed/subscriptions" target="_blank"><img src="http://cdn.stereogum.com/files/2012/02/Gorillaz1.jpg"></a></li>
+                <li><a href="http://www.icy-veins.com/wow/protection-paladin-pve-tank-guide" target="_blank"><img src="http://maxdps.com/images/section/4_2.jpg"></a></li>
+                </ul>
+            <div class="clearfix"></div>
+        </div>  
+        <div class="container">
+            <p id="favorite-2"> The Sagas of Apple Juice Ninja lolwut </p>
+            <p id="favorite-2"> Coming soon: erotic stories.  Just kidding.  More like failed attempts at speaking to women.  And stuff.  And things.
+            <p id="liam"> Friend: "We should make a synth pop band.  Make $10million.  Retire and pretend like we didn't do that."
+            <p id="liam"> "Capitalism?  More like CRAPITALISM, amiright?" -Karl Marx, probably
+            <a href="http://cache.bordom.net/server/?type=image&size=full&id=aHR0cDovL2Noem1lbWViYXNlLmZpbGVzLndvcmRwcmVzcy5jb20vMjAxMi8wMy9pbnRlcm5ldC1tZW1lcy1zaGF1bi1vZi10aGUtdWdhbmRlYWQ0LmpwZw%3D%3D" target="_blank"><p id="liam"> Master Plan for saving Africa </p></a>
+            <a href=""
+        <?php $contents = ob_get_clean();
+        echo str_ireplace("haste", "parry", $contents); ?>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
