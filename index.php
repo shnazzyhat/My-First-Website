5fd3be1a36ed08a8a0b0ca11b7b250a76e5e524d
shnazzyhat <juicycap@gmail.com>
2014-10-22 14:24:32 -0400

    typos

--- index.php
+++ index.php
@@ -42,8 +42,9 @@
             <p id="josh"> Using this service, you can upload your portfolio and give anyone specific access to it. </p>
             <p id="josh"> Your portfolio can also be protected from random views.  If they dont have permission from you, they can't see it. </p>
             <p id="josh"> This website also serves as a job request host.  Look up the kind of work you want done and a list of artists will appear for you. </p>
-            <p id="josh"> You can then request their portfolios or send in job descriptions and find the person who is right for you. </p>    
-            <p id="josh"> Click here to get started!
+            <p id="josh"> You can then request their portfolios or send in job descriptions and find the person who is right for your job. </p>    
+            <p id="josh"> Click here to sign up and get started!
+            <p id="josh"> Already a member? Click here.
         </div>
         <?php $contents = ob_get_clean();
         echo str_ireplace("haste", "parry", $contents); ?>
