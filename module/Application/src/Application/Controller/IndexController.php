91f5b52770449930b5ef784194fa82b33d43d473
shnazzyhat <juicycap@gmail.com>
2014-10-22 16:18:25 -0400

    Did lots of stuff in Zend like add models and normalize design

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -11,11 +11,68 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Models\Portfolio;
+use Zend\Db\Adapter\Adapter as DbAdapter;
+use Zend\Db\Sql\Sql;
+use Zend\Db\ResultSet\ResultSet;
+
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+
+    	/*
+    	$adapter = new DbAdapter(array(
+    		'driver' => 'mysqli',
+    		'username' => 'root',
+    		'database' => 'liamdonpain'
+    	));
+
+    	$sql = new Sql($adapter);
+    	$select = $sql->select();
+    	$select->from('users');
+    	$select->where(array(1 => 1));
+
+    	$statement = $sql->prepareStatementForSqlObject($select);
+    	$results = $statement->execute();
+
+    	$resultSet = new ResultSet();
+    	$resultSet->initialize($results);
+
+
+    	foreach( $resultSet as $row ) {
+    		print_r($row);
+    	}
+    	//this will get us all the users in our table and they are now inside the results variable
+
+    	// i know this is a bit complicated im just trying to illustrate a point
+		*/
+		
+    	$chelsea = new Portfolio();
+    	$chelsea->setOwner('chelsea');
+    	$chelsea->setPieces(100);
+    	$chelsea->setMedium('Toy Design');
+
+    	$liam = new Portfolio();
+    	$liam->setOwner('liam');
+    	$liam->setPieces(100);
+    	$liam->setMedium('Writing');
+
+    	$eric = new Portfolio();
+    	$eric->setOwner('eric');
+    	$eric->setPieces(-200102);
+    	$eric->setMedium('Pally Tanking');
+    	$eric->setFavoriteArtist('Theck');
+
+    	$stephen = new Portfolio();
+    	$stephen->setOwner('Stephen');
+    	$stephen->setPieces(2);
+    	$stephen->setMedium('Graphic Design');
+
+        return new ViewModel(array(
+        	'bae' => "Chelsea",
+        	'portfolios' => array($chelsea, $liam, $eric, $stephen)
+        ));
     }
 }
---
+++ module/Application/src/Application/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application/Models/User;
+
+class User {
+	
+	private $name;
+	private $email;
+
+	public function findByEmail( $emailToUse ) {
+		
+	}
+}
