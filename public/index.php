1b927747adaf218d4051b8c22abd38980fbac5f8
Stephen Parente <webmakersteve@gmail.com>
2015-01-01 21:28:55 -0500

    Removed Zend

--- public/index.php
+++ public/index.php
@@ -1,17 +1,30 @@
-<?php
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
+<?php echo $this->doctype(); ?>
 
-// Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
-}
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Liam the DonPain - Website of the gods</title>
 
-// Setup autoloading
-require 'init_autoloader.php';
+  <!-- Le styles -->
+  <link href="/css/style.css" rel="stylesheet" type="text/css">
 
-// Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+</head>
+<body>
+
+  <div class="container">
+    <h1>My Bae is Chelsea</h1>
+    <h2>Portfolios</h2>
+
+
+    <p id="josh"> An organized and easy to access protfolio is key for any deisgner or artist. </p>
+    <p id="josh"> Using this service, you can upload your portfolio and give anyone specific access to it. </p>
+    <p id="josh"> Your portfolio can also be protected from random views.  If they dont have permission from you, they can't see it. </p>
+    <p id="josh"> This website also serves as a job request host.  Look up the kind of work you want done and a list of artists will appear for you. </p>
+    <p id="josh"> You can then request their portfolios or send in job descriptions and find the person who is right for your job. </p>
+    <p id="josh"> Click here to sign up and get started!
+    <p id="josh"> Already a member? Click here.
+  </div>
+
+</body>
+
+</html>
